305bdaab6ca2581b6d6c4451544d8ac715d27917
Alexandre Machado <machado_silva@hotmail.com>
2020-07-04 07:27:29 -0400

    Termino do projeto

--- src/applications/Program.java
+++ src/applications/Program.java
@@ -1,9 +1,24 @@
 package applications;
 
+import java.util.Date;
+
+import entities.Order;
+import entities.enums.OrderStatus;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMANT);
+		
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED;
+		
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
 
 	}
 
